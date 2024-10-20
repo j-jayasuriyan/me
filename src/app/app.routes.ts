17c6c5289b5574d4c7e8e9701c7bee4a5baa020e
Jayasuriyan <jayasuriyan@presidio.com>
2024-10-20 18:28:25 +0530

    Added Maintenance Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent , title: 'Home'},
+  { path: 'about', component: AboutComponent, title: 'About' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
